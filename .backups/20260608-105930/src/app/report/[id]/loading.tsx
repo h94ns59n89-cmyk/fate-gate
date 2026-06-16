044027cf9a192632d4e62d2323ebbddb0ac66833
developer <dev@example.com>
2026-06-16 11:35:49 +0800

    chore: ignore backups and dev logs

--- .backups/20260608-105930/src/app/report/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from '@/components/common/LoadingSpinner';
-
-export default function ReportLoading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-brand">
-      <LoadingSpinner size="lg" />
-    </div>
-  );
-}
